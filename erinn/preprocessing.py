fd939cc35baa2c7205aec22ed4317c4705f06e9e
yezhengkai <supon3060@gmail.com>
2020-03-20 16:35:12 +0800

    Fix make_processed_dataset, add to_section and refactor

--- erinn/preprocessing.py
+++ erinn/preprocessing.py
@@ -4,12 +4,19 @@ from __future__ import division
 from __future__ import generator_stop
 from __future__ import print_function
 
+import multiprocessing as mp
 import os
 import re
+from functools import partial
 
 import numba
 import numpy as np
 import tensorflow as tf
+from tqdm import tqdm
+
+from erinn.utils.io_utils import read_config_file
+from erinn.utils.io_utils import read_pkl
+from erinn.utils.io_utils import write_pkl
 
 
 def log_transform(arr, inverse=False, inplace=True):
@@ -75,9 +82,8 @@ def log_transform(arr, inverse=False, inplace=True):
 
 
 @numba.njit()
-def add_noise(x, ratio=0.1):
-    """
-    Add noise to each element of the array by a certain percentage.
+def add_noise(x, scale=0.05, noise_type='normal'):
+    """Add noise to each element of the array by a certain percentage.
 
     In order to handle large arrays under memory constraints, this function uses in-place mode.
 
@@ -85,8 +91,13 @@ def add_noise(x, ratio=0.1):
     ----------
     x : numpy.ndarray
         Array that you wanted to add noise.
-    ratio : float, default 0.05
-        Noise added to element is proportional to this value.
+    scale : float, default 0.05
+        If noise_type is 'normal', scale is represent the standard deviation.
+        If noise_type is 'uniform', the noise added to element is proportional to this variable.
+    noise_type: str, {'normal', 'uniform'}, default normal
+        Noise type.
+        "normal" indicates that the noise is sampled from a Gaussian probability distribution function.
+        "uniform" indicates that the noise is sampled from a uniform probability distribution function.
 
     Returns
     -------
@@ -94,22 +105,35 @@ def add_noise(x, ratio=0.1):
 
     References
     ----------
-    https://stackoverflow.com/questions/44257931/fastest-way-to-add-noise-to-a-numpy-array
+    .. [1] https://stackoverflow.com/questions/44257931/fastest-way-to-add-noise-to-a-numpy-array
+    .. [2] https://github.com/simpeg/simpeg/blob/178b54417af0b892a3920685056a489ab2b6cda1/SimPEG/Survey.py#L501-L502
+    .. [3] https://stackoverflow.com/questions/14058340/adding-noise-to-a-signal-in-python/53688043#53688043
+    .. [4] https://numba.pydata.org/numba-doc/latest/reference/numpysupported.html
     """
-    x = x.reshape(-1)  # flat view
-    for i in range(len(x)):
-        x[i] += x[i] * np.random.uniform(-1, 1) * ratio
+    # Since version 0.28.0, the generator is thread-safe and fork-safe.
+    # Each thread and each process will produce independent streams of random numbers.
+    # x = x.reshape(-1)  # flat view
+    x = x.ravel()  # flat view
+
+    if noise_type == 'normal':
+        for i in range(len(x)):
+            x[i] += scale * abs(x[i]) * np.random.normal(0.0, 1.0)
+    elif noise_type == 'uniform':
+        for i in range(len(x)):
+            x[i] += scale * abs(x[i]) * np.random.uniform(-1.0, 1.0)
+    else:
+        raise(NotImplementedError('noise_type is not supported.'))
 
 
-def source_receiver_midpoints(SRCLOC, RECLOC):
+def source_receiver_midpoints(Tx_locations, Rx_locations):
     """
     Calculate source receiver midpoints.
 
     Parameters
     ----------
-    SRCLOC : numpy.ndarray
-        Source locations.
-    RECLOC : numpy.ndarray
+    Tx_locations : numpy.ndarray
+        Transmitter locations.
+    Rx_locations : numpy.ndarray
         Receiver locations.
 
     Returns
@@ -127,31 +151,38 @@ def source_receiver_midpoints(SRCLOC, RECLOC):
     # initialize midx and midz
     midx = []
     midz = []
-    for i in range(len(SRCLOC)):
-        Cmid = (SRCLOC[i, 0] + SRCLOC[i, 2]) / 2  # midpoint of the current electrode (Tx) pair (in x direction)
-        Pmid = (RECLOC[i, 0] + RECLOC[i, 2]) / 2  # midpoint of the potential electrode (Rx) pair (in x direction)
-        zsrc = (SRCLOC[i, 1] + SRCLOC[i, 3]) / 2  # midpoint of the current electrode (Tx) pair (in z direction)
-        midx.append(((Cmid + Pmid) / 2))  # midpoint of the Cmid and Pmid (x direction)
-        midz.append(np.abs(Cmid - Pmid) / 2 + zsrc)  # Half the length between Cmid and Pmid, then add zsrc (in z direction, positive down)
+    for i in range(len(Tx_locations)):
+        # midpoint of the current electrode (Tx) pair (in x direction)
+        Cmid = (Tx_locations[i, 0] + Tx_locations[i, 2]) / 2
+        # midpoint of the potential electrode (Rx) pair (in x direction)
+        Pmid = (Rx_locations[i, 0] + Rx_locations[i, 2]) / 2
+        # midpoint of the current electrode (Tx) pair (in z direction)
+        zsrc = (Tx_locations[i, 1] + Tx_locations[i, 3]) / 2
+        # midpoint of the Cmid and Pmid (x direction)
+        midx.append(((Cmid + Pmid) / 2))
+        # Half the length between Cmid and Pmid, then add zsrc (in z direction, positive down)
+        midz.append(np.abs(Cmid - Pmid) / 2 + zsrc)
     midx = np.array(midx).reshape(-1, 1)  # form an 2D array
     midz = np.array(midz).reshape(-1, 1)  # form an 2D array
 
     return midx, midz
 
 
-def to_midpoint(array, SRCLOC, RECLOC):
-    """
-    Reshape inputs tensor to midpoint image.
+def to_midpoint(array, Tx_locations, Rx_locations, value=0.0):
+    """Reshape inputs tensor to midpoint image.
+
     shape = (accumulated number of same midpoint, number of midpoint, 1)
 
     Parameters
     ----------
     array : numpy.ndarray
         The array you want to reshape.
-    SRCLOC : numpy.ndarray
-        Source locations.
-    RECLOC : numpy.ndarray
+    Tx_locations : numpy.ndarray
+        Transmitter locations.
+    Rx_locations : numpy.ndarray
         Receiver locations.
+    value : float
+        The value of the blank element you want to fill in.
 
     Returns
     -------
@@ -160,7 +191,7 @@ def to_midpoint(array, SRCLOC, RECLOC):
     """
 
     array = array.reshape(-1)  # flatten input arrays
-    midx, midz = source_receiver_midpoints(SRCLOC, RECLOC)  # calculate midpoint
+    midx, midz = source_receiver_midpoints(Tx_locations, Rx_locations)  # calculate midpoint
 
     unique_midx, index_midx = np.unique(midx, return_inverse=True)
     num_unique_midx = len(unique_midx)
@@ -177,25 +208,28 @@ def to_midpoint(array, SRCLOC, RECLOC):
     # pad the list of lists to form an array
     new_array = tf.keras.preprocessing.sequence.pad_sequences(new_array,
                                                               dtype='float64',
-                                                              padding='post')
+                                                              padding='post',
+                                                              value=value)
     new_array = np.expand_dims(new_array.T, axis=2)  # reshape to 3D array
 
     return new_array
 
 
-def to_txrx(array, SRCLOC, RECLOC):
-    """
-    Reshape inputs tensor to Tx-Rx image.
+def to_txrx(array, Tx_locations, Rx_locations, value=0.0):
+    """Reshape inputs tensor to Tx-Rx image.
+
     shape = (number of Tx pair, number of Rx pair, 1)
 
     Parameters
     ----------
     array : numpy.ndarray
         The array you want to reshape.
-    SRCLOC : numpy.ndarray
-        Source locations.
-    RECLOC : numpy.ndarray
+    Tx_locations : numpy.ndarray
+        Transmitter locations.
+    Rx_locations : numpy.ndarray
         Receiver locations.
+    value : float
+        The value of the blank element you want to fill in.
 
     Returns
     -------
@@ -205,10 +239,14 @@ def to_txrx(array, SRCLOC, RECLOC):
 
     array = array.reshape(-1)  # flatten input arrays
     # find unique Tx pair and unique Rx pair
-    unique_srcloc, index_src = np.unique(SRCLOC, return_inverse=True, axis=0)
-    unique_recloc, index_rec = np.unique(RECLOC, return_inverse=True, axis=0)
-    num_index = len(index_src)  #
-    new_array = np.zeros((unique_srcloc.shape[0], unique_recloc.shape[0]))
+    unique_Tx_locations, index_src = np.unique(Tx_locations, return_inverse=True, axis=0)
+    unique_Rx_locations, index_rec = np.unique(Rx_locations, return_inverse=True, axis=0)
+
+    # create new zero array and assign value
+    num_index = len(index_src)
+    new_array = np.ones((unique_Tx_locations.shape[0],
+                         unique_Rx_locations.shape[0]),
+                        dtype=np.float) * value
     for i in range(num_index):
         new_array[index_src[i], index_rec[i]] = array[i]
 
@@ -217,8 +255,37 @@ def to_txrx(array, SRCLOC, RECLOC):
     return new_array
 
 
-# TODO: When deprecating npz support, we should extract the import command.
-# TODO: improve import statement, use multiprocess
+def to_section(array, nCx, nCy):
+    """Reshape inputs tensor to section image.
+
+    shape = (
+        number of cell center mesh in the z (y) direction,
+        number of cell center mesh in the x direction,
+        1
+    )
+
+    Parameters
+    ----------
+    array : numpy.ndarray
+        The array you want to reshape.
+    nCx : int
+        Number of cell center mesh in the x direction.
+    nCy : int
+        Number of cell center mesh in the z (y) direction.
+
+    Returns
+    -------
+    new_array : numpy.ndarray
+        Reshaped array.
+    """
+    array = array.reshape(-1)  # flatten input arrays
+    new_array = np.flipud(array.reshape((nCy, nCx)))
+    new_array = np.expand_dims(new_array, axis=2)  # reshape to 3D array
+
+    return new_array
+
+
+# TODO: use tfRecord
 def make_processed_dataset(config_file):
     """
     Preprocess raw dataset and save it to processed directory.
@@ -232,49 +299,142 @@ def make_processed_dataset(config_file):
     -------
     None
     """
-    from erinn.utils.io_utils import read_config_file, read_pkl, write_pkl
 
     config = read_config_file(config_file)
+    raw_data_dir = config['raw_data_dir']
+    save_processed_data_dir = config['save_processed_data_dir']
     preprocess = config['preprocess']
+    simulator_pkl = os.path.join(raw_data_dir, 'simulator.pkl')
+    save_simulator_pkl = os.path.join(save_processed_data_dir, 'simulator.pkl')
     do_preprocess = any(value['perform'] for action, value in preprocess.items())
-    # read SRCLOC and RECLOC
-    if preprocess['to_midpoint']['perform']\
-            or preprocess['to_txrx']['perform']:
-        glob_para = read_pkl(config['glob_para_pkl'])
-        SRCLOC = glob_para['SRCLOC']
-        RECLOC = glob_para['RECLOC']
+
+    simulator = read_pkl(simulator_pkl)
+    # read nCx and nCy
+    nCx = simulator.mesh.nCx  # number of cell center mesh in the x direction
+    nCy = simulator.mesh.nCy  # number of cell center mesh in the z (y) direction
+    # read Tx_locations and Rx_locations
+    Tx_locations = simulator.urf.abmn_locations[:, :4]
+    Rx_locations = simulator.urf.abmn_locations[:, 4:]
+    # expand simulator.config and save it
+    simulator.config = {
+        'generate': simulator.config,  # config for generate data
+        'preprocess': config  # config for preprocess data
+    }
+    os.makedirs(save_processed_data_dir, exist_ok=True)
+    write_pkl(simulator, save_simulator_pkl)
 
     if do_preprocess:
-        raw_data_dir = config['raw_data_dir']
-        processed_data_dir = config['processed_data_dir']
-        pattern = re.compile(raw_data_dir)
-        pattern_name = r'raw'
-        replace_name = r'processed'
+        pattern_raw_pkl = re.compile('raw_data_\d{6}.pkl')
 
         for root_dir, sub_dirs, files in os.walk(raw_data_dir):
-            for filename in files:
-                if filename.endswith('pkl'):
-                    pkl_name = os.path.join(root_dir, filename)
-                    sub_dir_in_processed = re.sub(pattern, processed_data_dir, root_dir)
-                    new_pkl_name = os.path.join(sub_dir_in_processed, re.sub(pattern_name, replace_name, filename))
-                    data = read_pkl(pkl_name)
+            # filter files list so the files list will contain pickle files that match the pattern
+            files = list(filter(pattern_raw_pkl.match, files))
+            # If the files list is empty, continue to the next iteration of the loop
+            if not files:
+                continue
+            # make sub directory
+            sub_dir_in_processed = re.sub(raw_data_dir, save_processed_data_dir, root_dir)
+            os.makedirs(sub_dir_in_processed, exist_ok=True)
 
-                    # preprocess
-                    for k, v in preprocess.items():
-                        if k == 'add_noise' and v.get('perform'):
-                            add_noise(data['inputs'], **v.get('kwargs'))
-                        elif k == 'log_transform' and v.get('perform'):
-                            log_transform(data['inputs'], **v.get('kwargs'))
-                        elif k == 'to_midpoint' and v.get('perform'):
-                            data['inputs'] = to_midpoint(data['inputs'], SRCLOC, RECLOC)
-                        elif k == 'to_txrx' and v.get('perform'):
-                            data['inputs'] = to_txrx(data['inputs'], SRCLOC, RECLOC)
+            # Parallel version!
+            par = partial(
+                _make_processed_dataset,
+                preprocess=preprocess,
+                root_dir=root_dir,
+                sub_dir_in_processed=sub_dir_in_processed,
+                Tx_locations=Tx_locations, Rx_locations=Rx_locations,
+                nCx=nCx, nCy=nCy
+            )
+            pool = mp.Pool(processes=mp.cpu_count(), maxtasksperchild=1)
+            for data in tqdm(pool.imap_unordered(par, files),
+                             desc=f'Preprocess data and save to {sub_dir_in_processed}',
+                             total=len(files)):
+                pass
+            pool.close()
+            pool.join()
 
-                    # save pickle in processed dir
-                    os.makedirs(sub_dir_in_processed, exist_ok=True)
-                    write_pkl(data, new_pkl_name)
+            # Serial version!
+            # for filename in files:
+            #     pkl_name = os.path.join(root_dir, filename)
+            #     data = read_pkl(pkl_name)
+            #     # check if the data is dict and have "resistance" and "resistivity_log10" keys
+            #     if (not isinstance(data, dict)
+            #             or data.get('resistance') is None
+            #             or data.get('resistivity_log10') is None):
+            #         continue
+
+            #     # preprocess
+            #     for k, v in preprocess.items():
+            #         if k == 'add_noise' and v.get('perform'):
+            #             add_noise(data['resistance'], **v.get('kwargs'))
+            #         elif k == 'log_transform' and v.get('perform'):
+            #             log_transform(data['resistance'], **v.get('kwargs'))
+            #         elif k == 'to_midpoint' and v.get('perform'):
+            #             data['resistance'] = to_midpoint(
+            #                 data['resistance'], Tx_locations, Rx_locations
+            #             )
+            #         elif k == 'to_txrx' and v.get('perform'):
+            #             data['resistance'] = to_txrx(
+            #                 data['resistance'], Tx_locations, Rx_locations
+            #             )
+            #         elif k == 'to_section' and v.get('perform'):
+            #             data['resistivity_log10'] = to_section(
+            #                 data['resistivity_log10'], nCx, nCy
+            #             )
+
+            #     # save pickle in processed dir
+            #     new_pkl_name = os.path.join(
+            #         sub_dir_in_processed,
+            #         re.sub(r'raw', r'processed', filename)
+            #     )
+            #     write_pkl(data, new_pkl_name)
+
+        # show information about input / target tensor shape
         try:
-            print(f"The shape of inputs: {data['inputs'].shape}")
-            print(f"The shape of targets: {data['targets'].shape}")
+            print("The shape of resistance (shape of NN input data): "
+                  + f"{data['resistance'].shape}")
+            print("The shape of resistivity (shape of NN target data): "
+                  + f"{data['resistivity_log10'].shape}")
+            print("IF YOU WANT TO GET THE RAW resistivity_log10, YOU SHOULD USE"
+                  + " `raw_resistivity_log10 = np.flipud(resistivity_log10).flatten()`")
         except NameError as err:
             pass  # no pickle files
+
+
+def _make_processed_dataset(filename, preprocess, root_dir, sub_dir_in_processed,
+                            Tx_locations, Rx_locations, nCx, nCy):
+    # for filename in files:
+    pkl_name = os.path.join(root_dir, filename)
+    data = read_pkl(pkl_name)
+    # check if the data is dict and have "resistance" and "resistivity_log10" keys
+    if (not isinstance(data, dict)
+            or data.get('resistance') is None
+            or data.get('resistivity_log10') is None):
+        raise Exception('data is not a dict or dict does not contain essential keys')
+
+    # preprocess
+    for k, v in preprocess.items():
+        if k == 'add_noise' and v.get('perform'):
+            add_noise(data['resistance'], **v.get('kwargs'))
+        elif k == 'log_transform' and v.get('perform'):
+            log_transform(data['resistance'], **v.get('kwargs'))
+        elif k == 'to_midpoint' and v.get('perform'):
+            data['resistance'] = to_midpoint(
+                data['resistance'], Tx_locations, Rx_locations
+            )
+        elif k == 'to_txrx' and v.get('perform'):
+            data['resistance'] = to_txrx(
+                data['resistance'], Tx_locations, Rx_locations
+            )
+        elif k == 'to_section' and v.get('perform'):
+            data['resistivity_log10'] = to_section(
+                data['resistivity_log10'], nCx, nCy
+            )
+
+    # save pickle in processed dir
+    new_pkl_name = os.path.join(
+        sub_dir_in_processed,
+        re.sub(r'raw', r'processed', filename)
+    )
+    write_pkl(data, new_pkl_name)
+    return data
